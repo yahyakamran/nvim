4d01444b65a7b2ddc1af3f95fe3561bf4b61b19b
yahyakamran <yahyakamran099@gmail.com>
2023-12-27 03:12:09 +0500

    letsgoo

---
+++ init.lua
@@ -0,0 +1,100 @@
+--Packer
+vim.cmd [[packadd packer.nvim]]
+require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'nvim-treesitter/nvim-treesitter'
+    require'nvim-treesitter.configs'.setup {
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+    highlight = {
+        enable = true,
+    }}
+    use 'junegunn/fzf'
+    use 'junegunn/fzf.vim'
+    use "rebelot/kanagawa.nvim"
+    use 'mbbill/undotree'
+    use "tpope/vim-fugitive"
+    vim.cmd("colorscheme kanagawa")
+    -- LSP:
+    use {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v3.x',
+      requires = {
+        {'williamboman/mason.nvim'},
+        {'williamboman/mason-lspconfig.nvim'},
+        {'neovim/nvim-lspconfig'},
+        {'hrsh7th/nvim-cmp'},
+        {'hrsh7th/cmp-nvim-lsp'},
+        {'L3MON4D3/LuaSnip'},
+    }}
+ end)
+--Some VIM PRESETS
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.guicursor = ""
+vim.opt.wrap = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.scrolloff = 8
+vim.opt.list = true
+vim.opt.listchars:append('tab:> ')
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.cursorline = true
+vim.opt.swapfile = false
+-- key maps
+vim.g.mapleader = " "
+-- undotree
+vim.keymap.set('n','<leader>u' , vim.cmd.UndotreeToggle)
+--fzf finder
+-- Open recently used files
+vim.api.nvim_set_keymap("n", "<leader>fr", ":History<CR>", { noremap = true })
+-- Open files in same directory as current file
+vim.api.nvim_set_keymap("n", "<leader>ff", ":e %:h/<C-d>", { noremap = true })
+--vim-fugitive
+vim.keymap.set('n','<leader>gc' , vim.cmd.Git)
+
+--LSP CONFIG
+local lsp_zero = require('lsp-zero')
+lsp_zero.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {'tsserver','clangd'},
+  handlers = {
+    lsp_zero.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
+  }
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
+cmp.setup({
+  sources = {
+    {name = 'path'},
+    {name = 'nvim_lsp'},
+    {name = 'nvim_lua'},
+  },
+  formatting = lsp_zero.cmp_format(),
+  mapping = cmp.mapping.preset.insert({
+    ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }),
+})
